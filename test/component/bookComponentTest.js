95003c376498f8c1d0f6735bcf758d0b61460009
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-25 12:58:41 +0200

    prg

--- test/component/bookComponentTest.js
+++ test/component/bookComponentTest.js
@@ -1,9 +1,12 @@
 const httpClient = require('supertest');
 const app = require('../../src/app');
+const assert = require('assert');
 
 describe('Book inventory', function () {
-    it('allows to stock up the items', function (done) {
-        httpClient(app)
+    it('allows to stock up the items', async function (done) {
+        const request = httpClient(app);
+
+        const createResult = await request
             .post('/book')
             .send({
                 title: "JavaScript in Action",
@@ -12,11 +15,15 @@ describe('Book inventory', function () {
                 description: "The ultimate JS book!"
             })
             .set('Content-Type', 'application/json')
-            .expect(200, {
-                title: "JavaScript in Action",
-                authors: ["James Smith", "Kate Donovan"],
-                isbn: "0123456789",
-                description: "The ultimate JS book!"
-            }, done);
+            .expect(302);
+
+        const readResult = await request.get(createResult.header.location)
+
+        assert.deepStrictEqual(readResult.body, {
+            title: "JavaScript in Action",
+            authors: ["James Smith", "Kate Donovan"],
+            isbn: "0123456789",
+            description: "The ultimate JS book!"
+        });
     })
 });
