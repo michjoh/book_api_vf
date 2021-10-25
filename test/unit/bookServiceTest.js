ebc355596140c505d51ab7939f7f17780cc64c37
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-25 15:18:15 +0200

    service test prep

---
+++ test/unit/bookServiceTest.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+// const bookServiceFactory = require('../../src/bookService');
+
+describe('Book service', function () {
+    it('create or update happy path',  function () {
+
+    });
+});
