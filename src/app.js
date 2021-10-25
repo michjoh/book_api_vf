b970104fc1c38aecdb8e7ba5a84f5644eaa2adc5
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-25 12:30:43 +0200

    book routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const MongoClient = require('mongodb').MongoClient;
-const app = express();
+const bookRoutes = require("./bookRoutes");
 
-const url = 'mongodb://localhost:27017/booksapi';
+const app = express();
 
 function log(req, res, next) {
     // log incoming request
@@ -20,39 +19,8 @@ app.use(auth);
 app.get("/", auth, function (req, res) {
     res.send("Hello World!");
 });
+app.use("/", bookRoutes);
 
-const booksPromise = MongoClient.connect(url).then(function (client) {
-    return client.db().collection("books");
-});
-
-
-app.get("/book/:isbn", async function (req, res, next) {
-    const isbn = req.params.isbn;
-    try {
-        const books = await booksPromise;
-        const book = await books.findOne(
-            {isbn},
-            {projection: {_id: 0}}
-        );
-        res.json(book);
-    } catch (e) {
-        next(e);
-    }
-
-});
-app.post("/book", async function (req, res, next) {
-    const {title, authors, isbn, description} = req.body;
-    try {
-        const books = await booksPromise;
-        await books.updateOne(
-            {isbn: isbn},
-            {$set : {title, authors, isbn, description} },
-            {upsert: true});
-        res.json({title, authors, isbn, description});
-    } catch (e) {
-        next(e);
-    }
-});
 
 app.use(function notFound(req, res, next) {
     const err = new Error("Not Found");
