69b9c2f311718ced7107f7e5f57e6ba6295e44e3
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-25 14:25:20 +0200

    validate

--- src/bookRoutes.js
+++ src/bookRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const controller = require("./bookController");
 const validateBook = require("./validateBookMiddleware");
 
-router.post("/book", validateBook, controller.createOrUpdate);
+router.post("/book", validateBook,  controller.createOrUpdate);
 router.get("/book/:isbn", controller.details);
 
 module.exports = router;
