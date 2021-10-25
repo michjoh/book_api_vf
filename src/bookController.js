7ee9d55bf40beb0a8585b5c90a0f5b22bda30ee4
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-25 16:03:58 +0200

    error boilerplate

--- src/bookController.js
+++ src/bookController.js
@@ -1,24 +1,28 @@
-// Dependency Injection - passing args to fns
-module.exports = ({bookService, bookRepository}) => ({
+const mapValues = require("lodash.mapvalues");
+
+// const mapValues = (api, f) => Object.fromEntries(Object.entries(api).map(([key, value]) => [key, f(value)]));
+
+
+function withErrorHandling(api) {
+    return mapValues(api, wrapWithTryCatch);
+}
+
+const wrapWithTryCatch = (fn) => {
+    return async function (req, res, next) {
+        return Promise.resolve(fn(req, res, next)).catch(next);
+    };
+};
+
+
+module.exports = ({bookService, bookRepository}) => withErrorHandling({
     async createOrUpdate(req, res, next) {
-        // HTTP
         const {title, authors, isbn, description} = req.body;
-        try {
-            // JS
-            await bookService.createOrUpdate({title, authors, isbn, description});
-            // HTTP
-            res.redirect(`/book/${isbn}`);
-        } catch (e) {
-            next(e);
-        }
+        await bookService.createOrUpdate({title, authors, isbn, description});
+        res.redirect(`/book/${isbn}`);
     },
     async details(req, res, next) {
-        try {
-            const isbn = req.params.isbn;
-            const book = await bookRepository.findOne(isbn);
-            res.json(book);
-        } catch(e) {
-            next(e);
-        }
+        const isbn = req.params.isbn;
+        const book = await bookRepository.findOne(isbn);
+        res.json(book);
     }
 });
